6659695e0ceba04f212accaec63b74a22e5250df
David Godwin <dgodwin26@yahoo.co.uk>
2019-12-23 23:19:58 +0000

    Add checks to stringToLocation. Stub locationToString function. Am now checking if a location exists in a mapping. If a key doesn’t exist, the value returned for that position will be [-1, -1]. This overwrites Go’s default [0,0] because this is a legitimate position (A1) on the board. Also starting to return errors from functions which means I can start using if err != nil when I’m calling functions.

--- threegophers.go
+++ threegophers.go
@@ -1,5 +1,7 @@
 package main
-import ("fmt")
+import (
+	"errors"
+		)
 
 // Creating global variables + board outside of any function. 
 // These variables are now accessible to all functions in this file.
@@ -14,29 +16,41 @@ var board = [][]string {{e, e, e, e, g},
 				{e, e, e, e, e},
 				{g, e, e, e, e}}
 
-func stringToLocation(s string) ([2]int){
-	//TODO: add a check in case the location added isn't legal. 
-
+func stringToLocation(s string) ([2]int, error) {
 	//Takes a string s (eg "A1") and returns an integer array (eg [0,0])
+
+	//declare some maps for position [0] and [1] from the string.
 	m0 := make(map[byte]int)
+	m1 := make(map[byte]int)
+
+	//make the mapping
 	m0['A'] = 0
 	m0['B'] = 1
 	m0['C'] = 2
 	m0['D'] = 3
 	m0['E'] = 4
-
-	m1 := make(map[byte]int)
+	
 	m1['1'] = 0
 	m1['2'] = 1
 	m1['3'] = 2
 	m1['4'] = 3
 	m1['5'] = 4
-	return [2]int{m0[s[0]],m1[s[1]]}
+
+	_, ok1 := m0[s[0]]
+	_, ok2 := m1[s[1]]
+	//if the ok value is false, it means the key (_) wasn't in the map.
+	if ok1 == false || ok2 == false {
+		return [2]int{-1,-1}, errors.New("You supplied a position that isn't on the board")
+	}
+
+	return [2]int{m0[s[0]],m1[s[1]]}, nil
+}
+
+func locationToString(location [2]int)(string){
+	return ""
 }
 
 func main(){
-	fmt.Println(stringToLocation("A1")) //[0,0]
-	fmt.Println(stringToLocation("F1")) //needs to error
-	fmt.Println(stringToLocation("A8")) //needs to error
 	
 }
+
